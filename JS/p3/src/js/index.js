052e81019a7726e0c23c9f22420d2559972c910d
sperrodj <127434473+sperrodj@users.noreply.github.com>
2023-06-07 16:49:02 +0100

    Update index.js

--- JS/p3/src/js/index.js
+++ JS/p3/src/js/index.js
@@ -3,9 +3,14 @@ window.onload = () => {
 }
 
 function getData() {
+  let nombre = document.getElementById("name")
   document.querySelector('form').addEventListener('submit', e => {
     e.preventDefault()
     const data = Object.fromEntries(new FormData(e.target))
     console.log(JSON.stringify(data))
+    /* 1ª Modificación: Cuando el usuario inserta un número superior a 6 caracteres, salta una alerta */
+    if(nombre.value.length < 6) {
+      alert("Introduzca un nombre mayor a 6 caracteres")
+    } 
   })
 }
